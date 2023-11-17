e6dc1591abd0dc0d24898489644d602d738a9883
Sagar Nirwal <62455271+sn247776@users.noreply.github.com>
2023-11-18 02:27:00 +0530

    little changes

--- models/diet.js
+++ models/diet.js
@@ -1,7 +1,11 @@
 import mongoose from "mongoose";
 
 const schema = new mongoose.Schema({
- 
+  title: {
+    type: String,
+    required: true,
+  },
+
   dietpref: {
     type: String,
     required: true,
